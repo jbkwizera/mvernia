6cfa61dba34e511a6405d161f2d58f78276c05e0
meraxex <jeanbaptistekwi@gmail.com>
2020-04-01 00:34:58 -0700

    rename file

---
+++ commenting.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Commenting</title>
+        <link rel="stylesheet" href="./css/master.css">
+        <link rel="stylesheet" href="./css/home.css">
+        <link rel="stylesheet" href="./css/commenting.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+        <div class="main">
+            <div class="header">
+                <div class="comment-count">3 Comments</div>
+                <div class="comment-sort">
+                    <span>Sort by</span>
+                    <select>
+                        <option value="newest" selected>Newest</option>
+                        <option value="oldest">Oldest</option>
+                        <option value="engaging">Best</option>
+                    </select>
+                </div>
+            </div>
+            <div class="comment-main new-comment-main row">
+                <div class="photo-canvas col-md-1 col-sm-2 new-photo-canvas">
+                    <img src="./media/avatar.png" alt="">
+                </div>
+                <div class="col-md-11 col-sm-10 text-area new-text-area">
+                    <textarea name="comment" rows="3" placeholder="Comment..."></textarea>
+                    <div class="post-btn-cont">
+                        <button type="submit">Post</button>
+                    </div>
+                </div>
+            </div>
+            <div class="comment-main row">
+                <div class="photo-canvas col-md-1 col-sm-2">
+                    <img src="./media/sheldon-main.png" alt="">
+                </div>
+                <div class="text-area col-md-11 col-sm-10">
+                    <div class="comment-header">
+                        <span class="name">Rumena Dish</span>
+                        <span class="middot-separator">&middot;</span>
+                        <span class="time">6 months ago</span>
+                    </div>
+                    <div class="comment-text">
+                        The comment sytem you choose it all your choice. As stated in this article,
+                        refrain from using just one type, like Facebook. Not everyone is
+                        on Facebook, myself being one of those lucky ones.
+                        Do not alienate your users. It is only your loss when you do.
+                        You potential visitor will simply move on without a second
+                        thought.
+                    </div>
+                    <div class="comment-widgets">
+                        <i class="fa fa-heart"></i><span class="middot-separator">&middot;</span>
+                        <i class="fa fa-reply"></i><span class="middot-separator">&middot;</span>
+                        <i class="fa fa-flag"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="comment-main row">
+                <div class="photo-canvas col-md-1 col-sm-2">
+                    <img src="./media/sheldon-main.png" alt="">
+                </div>
+                <div class="text-area col-md-11 col-sm-10">
+                    <div class="comment-header">
+                        <span class="name">Rumena Dish</span>
+                        <span class="middot-separator">&middot;</span>
+                        <span class="time">6 months ago</span>
+                    </div>
+                    <div class="comment-text">
+                        The comment sytem you choose it all your choice. As stated in this article,
+                        refrain from using just one type, like Facebook. Not everyone is
+                        on Facebook, myself being one of those lucky ones.
+                        Do not alienate your users. It is only your loss when you do.
+                        You potential visitor will simply move on without a second
+                        thought.
+                    </div>
+                    <div class="comment-widgets">
+                        <i class="fa fa-heart"></i><span class="middot-separator">&middot;</span>
+                        <i class="fa fa-reply"></i><span class="middot-separator">&middot;</span>
+                        <i class="fa fa-flag"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="comment-main row">
+                <div class="photo-canvas col-md-1 col-sm-2">
+                    <img src="./media/sheldon-main.png" alt="">
+                </div>
+                <div class="text-area col-md-11 col-sm-10">
+                    <div class="comment-header">
+                        <span class="name">Rumena Dish</span>
+                        <span class="middot-separator">&middot;</span>
+                        <span class="time">6 months ago</span>
+                    </div>
+                    <div class="comment-text">
+                        The comment sytem you choose it all your choice. As stated in this article,
+                        refrain from using just one type, like Facebook. Not everyone is
+                        on Facebook, myself being one of those lucky ones.
+                        Do not alienate your users. It is only your loss when you do.
+                        You potential visitor will simply move on without a second
+                        thought.
+                    </div>
+                    <div class="comment-widgets">
+                        <i class="fa fa-heart"></i><span class="middot-separator">&middot;</span>
+                        <i class="fa fa-reply"></i><span class="middot-separator">&middot;</span>
+                        <i class="fa fa-flag"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
